776944e643ebe208ae72a7e00a460885ebae6dee
kms973 <newto9973@gmail.com>
2024-05-08 11:47:37 +0900

    cc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,8 +21,6 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from './recipes/recipe.service';
 
 
-;;';';';'
-
 @NgModule({
   declarations: [
     AppComponent,
